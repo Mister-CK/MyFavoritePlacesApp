ebf508ca49c30fbdb0032d8aacfc35af06e674aa
Chris Kapinga <chris.kapinga@ordina.nl>
2023-01-21 22:26:49 +0100

    add initial screens allPlaces and Addplace and navigation between them

---
+++ components/places/PlaceItem.js
@@ -0,0 +1,14 @@
+import {View, Text, Image, Pressable} from 'react-native';
+
+const PlaceItem = ({place, onSelect}) => {
+  return (
+    <Pressable onPress={onSelect}>
+      <Image source={{uri: place.imageUri}}/>
+      <View>
+        <Text>{place.title}</Text>
+        <Text> {place.address}</Text>
+      </View>
+    </Pressable>
+  );
+};
+export default PlaceItem;
