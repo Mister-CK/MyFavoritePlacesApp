071b40e0755a4d2955852fe7c138f933b44dec8f
Chris Kapinga <chris.kapinga@ordina.nl>
2023-01-22 15:14:41 +0100

    add course code with broken api key
    I don't have a creditCard to get a key myself

--- components/places/LocationPicker.js
+++ components/places/LocationPicker.js
@@ -1,13 +1,25 @@
-import {View, StyleSheet, Alert, Text} from 'react-native';
-import OutlinedButton from '../UI/OutlinedButton';
-import {Colors} from '../../constants/colors';
-import {getCurrentPositionAsync, getForegroundPermissionsAsync, useForegroundPermissions} from 'expo-location';
-import {PermissionStatus} from 'expo-image-picker';
-const LocationPicker = () => {
-  const [locationPermissionInformation, requestPermission] = useForegroundPermissions()
+import { useState } from 'react';
+import { Alert, View, StyleSheet, Image, Text } from 'react-native';
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from 'expo-location';
 
-  const verifyPermissions = async () => {
-    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+import { Colors } from '../../constants/colors';
+import OutlinedButton from '../UI/OutlinedButton';
+import { getMapPreview } from '../../util/location';
+
+function LocationPicker() {
+  const [pickedLocation, setPickedLocation] = useState();
+
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
       const permissionResponse = await requestPermission();
 
       return permissionResponse.granted;
@@ -24,42 +36,71 @@ const LocationPicker = () => {
     return true;
   }
 
-  const getLocationHandler = async () => {
-    const hasPermission = await verifyPermissions()
-    if (!hasPermission) {
-      return
-    }
-    const location = await getCurrentPositionAsync()
-    console.log(location)
-  }
-  const pickOnMapHandler = () => {
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
 
+    if (!hasPermission) {
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
   }
-  return(
+
+  function pickOnMapHandler() {}
+
+  let locationPreview = <Text>No location picked yet.</Text>;
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.image}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+        }}
+      />
+    );
+  }
+
+  return (
     <View>
-      <View style={styles.mapPreview}><Text>LocationPreview Not Implemented</Text></View>
-        <View style={styles.actions}>
-          <OutlinedButton icon="location" onPress={getLocationHandler}>Locate User</OutlinedButton>
-          <OutlinedButton icon="map" onPress={pickOnMapHandler}>Pick On Map</OutlinedButton>
-        </View>
+      <View style={styles.mapPreview}>{locationPreview}</View>
+      <View style={styles.actions}>
+        <OutlinedButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutlinedButton>
+        <OutlinedButton icon="map" onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutlinedButton>
+      </View>
     </View>
-  )
+  );
 }
 
-export default LocationPicker
+export default LocationPicker;
+
 const styles = StyleSheet.create({
-  mapPreview:{
-    width:"100%",
-    height:200,
-    marginVertical:8,
-    justifyContent:"center",
-    alignItems:"center",
+  mapPreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: Colors.primary100,
-    borderRadius:4,
+    borderRadius: 4,
+    overflow: 'hidden'
   },
-  actions:{
-    flexDirection:"row",
-    justifyContent:"space-around",
-    alignItems:"center",
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
-})
+  image: {
+    width: '100%',
+    height: '100%',
+    // borderRadius: 4
+  },
+});
